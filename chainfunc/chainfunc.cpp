45538623ae16d595cffe420bd25d3141485a5251
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-05-15 16:50:03 +0700

    memanggil

---
+++ chainfunc/chainfunc.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class buku
+{
+	string judul;
+
+public:
+	buku setJudu1(string judul)
+	{
+		this->judul = judul;
+		return *this; // chain function
+	}
+	string getJudul()
+	{
+		return this->judul;
+	}
+} bukunya;
+int main()
+{// bukunya.setJudu1("Matematika"); 
+ // cout << bukunya.getJudu1(); 
+	cout << bukunya.setJudu1("Matematika").getJudul(); // chain function calls 
+	return 0;
+}
