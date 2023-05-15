d40f8766bdec222da83b44a1d20209158c197346
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-05-15 15:57:22 +0700

    pengembalian

--- mbrstaticfunc/mbrstaticfunc.cpp
+++ mbrstaticfunc/mbrstaticfunc.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
-#include <striing>
+#include <string>
 using namespace std;
 
 class mahasiswa {
 private:
-	static int nim
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setId();
+	void printAll();
+
+	static void setNim(int pNim) { nim = pNim; /*definsi function*/  }
+	static int getNim() { return nim; /*definisi function*/ }
+
+	mahasiswa(string pnama) : nama(pnama) { setId(); }
 };
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setId() {
+	id = ++nim;
+}
+
+void mahasiswa::printAll() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
